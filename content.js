2a826d27740aa9a06b2cc6bb59225ae36982e755
Angela Pan <angela.pan1992@gmail.com>
2026-05-18 19:55:05 -0400

    clean up

--- content.js
+++ content.js
@@ -1,14 +1,10 @@
-// Netflix Hide Pause Ads - Content Script
-
 function hidePauseAds() {
   document.querySelectorAll('[data-uia="pause-ad"]').forEach((el) => {
     el.style.setProperty("display", "none", "important");
   });
 }
 
-// Run immediately in case the element is already in the DOM
 hidePauseAds();
 
-// Watch for dynamically injected pause ads (Netflix is a SPA)
 const observer = new MutationObserver(hidePauseAds);
 observer.observe(document.body, { childList: true, subtree: true });
